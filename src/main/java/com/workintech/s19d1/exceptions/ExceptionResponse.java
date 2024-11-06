c6ede3459f8c31efc3d59cac29d506f45d67f26e
oztrkmrt <merttoztrk@gmail.com>
2024-11-06 15:13:20 +0300

    Mission complete

---
+++ src/main/java/com/workintech/s19d1/exceptions/ExceptionResponse.java
@@ -0,0 +1,16 @@
+package com.workintech.s19d1.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+public class ExceptionResponse {
+
+    private String message;
+    private int Status;
+    private LocalDateTime dateTime;
+
+}
